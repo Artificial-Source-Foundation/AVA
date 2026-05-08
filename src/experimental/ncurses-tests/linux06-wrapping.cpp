0c0cd77713339dd5c15b840a85dc5f5154d26854
Carlo Wood <carlo@alinoe.com>
2026-05-08 17:10:51 +0200

    Use a Margin in linux06-wrapping.cpp

--- src/experimental/ncurses-tests/linux06-wrapping.cpp
+++ src/experimental/ncurses-tests/linux06-wrapping.cpp
@@ -11,12 +11,14 @@ int main()
     terminal::Dimension const size{15, 20};
     terminal::Position const top_left{terminal_session.rows() - size.height(), 10};
 
-    terminal::Window window(size, top_left);
-    window.set_background(terminal_session.default_rendition());
-    window.set_border({});
+    terminal::Window parent_window(size, top_left);
+    parent_window.set_background(terminal_session.default_rendition());
+    parent_window.set_border({});
+    terminal::Window window = parent_window.derwin(terminal::Margin{.top = 1, .bottom = 1, .left = 1, .right = 1});
     auto long_line = u8"Dark red window: αβγ this line is longer than the width of the window.";
-    window.addstr({1, 2}, long_line);
+    window.addstr(long_line);
     terminal_session.stdscr().refresh();
+    parent_window.refresh();
     window.refresh();
     wch = terminal_session.get_wch();
   }

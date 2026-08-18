215200293a4023973318889c628dc2c93acd4193
Carlo Wood <carlo@alinoe.com>
2026-08-19 00:58:08 +0200

    Add terminal::Spacer::create.

--- src/ava/tui/terminal/Spacer.h
+++ src/ava/tui/terminal/Spacer.h
@@ -6,11 +6,17 @@ namespace ava::tui::terminal {
 
 class Spacer : public LayoutItem
 {
- public:
+ private:
   // Construct a Spacer.
-  Spacer(LayoutItem::Properties layout_properties = {.minimum_width = greedy}) : LayoutItem(layout_properties) { }
+  Spacer(LayoutItem&& layout_item) : LayoutItem(std::move(layout_item)) { }
 
-  AVA_DEBUG_PRINT_MEMBERS_ON
+ public:
+  static std::unique_ptr<Spacer> create(LayoutItem::Properties layout_properties = {.minimum_width = 1})
+  {
+    return std::unique_ptr<Spacer>(new Spacer(layout_properties));
+  }
+
+  AVA_DEBUG_PRINT_MEMBERS_ON_BASE(LayoutItem)
 };
 
 } // namespace ava::tui::terminal
